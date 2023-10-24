d6dd181c6c4d67890f0c7e725f51b4a144525940
Yuri Kim <ymail.kim@gmail.com>
2023-10-23 23:37:36 -0700

    added home route

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//       <h1>testing</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
-
   return (
-    <>
-      <h1>testing</h1>
-    </>
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
