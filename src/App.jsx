861d91bb297ba6a37b00a0ccd8ced16abac93b62
Yuri Kim <ymail.kim@gmail.com>
2023-10-25 19:57:34 -0700

    modified routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+// Components
+import Footer from './components/footer/Footer'
+import Navbar from './components/header/Navbar'
+// Pages
+import Home from './pages/home/Home'
+import Loading from './pages/loading/Loading'
+import ResultConfirmation from './pages/result-confirmation/ResultConfirmation'
+import FinalResults from './pages/final-results/FinalResults'
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/loading' element={<Loading />} />
+          <Route exact path='/result-confirmation' element={<ResultConfirmation />} />
+          <Route exact path='/finalresults' element={<FinalResults />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
